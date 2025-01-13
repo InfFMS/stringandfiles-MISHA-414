0cb9d49bf6fc8df233f785c90a83993c13d06508
Петрухин Михаил Дмитриевич <m.petrukhin@main.nsu.ru>
2025-01-13 14:13:01 +0700

    task1

--- task1.py
+++ task1.py
@@ -4,3 +4,8 @@
 # Общее количество слов во всем тексте файла.
 # Общее количество символов (включая пробелы).
 # Выведите полученную статистику на экран.
+with open("task1.txt", 'r', encoding='utf-8') as f:
+    f1=f.readlines()
+    print(sum(1 for line in f1))
+    print(sum(line.count(' ')-line.count('—') for line in f1))
+    print(sum(len(line) for line in f1))
