99fc8917284292c7f9181e364fd53d1ef0f4087d
MISHA-414 <misha1.petruhin@gmail.com>
2025-01-20 11:55:20 +0700

    task2

--- task2.py
+++ task2.py
@@ -3,3 +3,11 @@
 # Найдите и замените все вхождения слова "Python" на слово "Питон" (регистр учитывать).
 # Запишите обновленный текст в новый файл с другим именем.
 # Выведите на экран сообщение о количестве произведённых замен.
+with open('task2.txt', 'r', encoding='utf-8') as file:
+    string=file.read()
+    count_Python=string.count('Python')
+    print(count_Python)
+    string=string.replace('Python', 'Питон', count_Python)
+with open('task2.txt', 'w', encoding='utf-8') as file:
+    file.write(string)
+
